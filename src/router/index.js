028de42664a57c4f0581fcf147305ef46e08701d
isaak kbr <chrisisaak22@gmail.com>
2024-12-10 20:07:03 +0100

    Setting up basic endpoint routes with corresponding views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import PostView from "@/views/PostView.vue";
+import AddPostView from "@/views/AddPostView.vue";
+import TestView from "@/views/TestView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/posts/add",
+      name: "post-add",
+      component: AddPostView,
+    },
+    {
+      path: "/test",
+      name: "test",
+      component: TestView,
+    },
+    {
+      path: "/posts/:id",
+      name: "post-detail",
+      component: PostView,
+    },
+  ],
 });
 
 export default router;
